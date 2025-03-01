ef6bace6f7371270f6e63318dceb3f575c6f1470
Lukasz Szczygielski <l_szczygielski@o2.pl>
2025-03-01 17:24:36 +0100

    Add main

--- brand-website/src/App.jsx
+++ brand-website/src/App.jsx
@@ -2,22 +2,43 @@ import React from 'react'
 
 const App = () => {
   return (
-    <header>
-      <nav>
-        <div className="logo">
-          <img src="../public/images/logo.png" alt="logo" />
+    <div className="container">
+      <header>
+        <nav>
+          <div className="logo">
+            <img src="../public/images/logo.png" alt="logo" />
+          </div>
+          <ul>
+            <li>Menu</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          <div className="login">
+            <button className='login'>login</button>
+          </div>
+        </nav>
+      </header>
+      <main>
+        <div className="left">
+          <h1>
+            YOUR FEET
+            <br />
+            DESERVE
+            <br />
+            THE BEST
+          </h1>
+          <h3>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</h3>
+          <div className="buttons">
+              <button className="shop-now">Shop Now</button>
+              <button className="category">Category</button>
+          </div>
         </div>
-        <ul>
-          <li>Menu</li>
-          <li>Location</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-        <div className="login">
-          <button className='login'>login</button>
+        <div className="right">
+          <img src="../public/images/shoe.png" alt="shoe" />
         </div>
-      </nav>
-    </header>
+      </main>
+    </div>
   )
 }
 
