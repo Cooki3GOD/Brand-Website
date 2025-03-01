107a096e981c9822c6fb537c2b9430e13af4d523
Lukasz Szczygielski <l_szczygielski@o2.pl>
2025-03-01 17:06:29 +0100

    Add header

--- brand-website/src/App.jsx
+++ brand-website/src/App.jsx
@@ -2,9 +2,22 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <header>
+      <nav>
+        <div className="logo">
+          <img src="../public/images/logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>Menu</li>
+          <li>Location</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <div className="login">
+          <button className='login'>login</button>
+        </div>
+      </nav>
+    </header>
   )
 }
 
